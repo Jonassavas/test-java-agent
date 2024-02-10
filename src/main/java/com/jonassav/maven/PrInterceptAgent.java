b4f47684e5c15d30d98a787d16733b69c67691fb
Jonas Sävås <jonas.savas123@gmail.com>
2024-02-10 17:31:46 +0100

    Intercepting methods with string arguments with print in their name did not get in-method substitution to work, only to remove the System.out.println in all methods

--- src/main/java/com/jonassav/maven/PrInterceptAgent.java
+++ src/main/java/com/jonassav/maven/PrInterceptAgent.java
@@ -2,34 +2,28 @@ package com.jonassav.maven;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.asm.MemberSubstitution;
-import net.bytebuddy.implementation.FixedValue;
-import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import javax.swing.text.Element;
 import java.lang.instrument.Instrumentation;
-import java.lang.reflect.Method;
 
-import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.none;
 
 public class PrInterceptAgent {
 
     public static void premain(String agentArgs, Instrumentation instrumentation) {
-        new AgentBuilder.Default()
+        new AgentBuilder.Default().ignore(none())
                 .type(ElementMatchers.nameContains("Test")) // Match all classes
                 .transform((builder, type, classLoader, module, protectionDomain) ->
-                        builder.visit(Advice.to(PrintInterceptor.class).on(ElementMatchers.takesArgument(0, String.class))))
+                        builder.visit(Advice.to(PrintInterceptor.class).on(ElementMatchers.takesArgument(0, String.class)
+                                .and(ElementMatchers.nameContainsIgnoreCase("print")))))
                 .installOn(instrumentation);
     }
 
     public static class PrintInterceptor {
         @Advice.OnMethodEnter
         public static void intercept(@Advice.Argument(value = 0, readOnly = false) String message) {
-            String modifiedMessage = "Modified: " + message;
-            System.out.println(modifiedMessage);
-            message = modifiedMessage;
+            message = "AGENT MODIFIED: " + message;
         }
     }
 }
